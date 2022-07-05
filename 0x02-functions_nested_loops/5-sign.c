0439caa726237474e1ec746df99e47accbf0127b
Vera Aninakwah <vaykeys0112@gmail.com>
2022-07-05 15:54:16 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ *
+ *   * @n: The number to be checked
+ *
+ *    *
+ *
+ * Return: 1, 0 -1
+ *
+ */
+
+int print_sign(int n)
+
+{
+
+	if (n > 0)
+	{
+
+
+
+			_putchar('+');
+
+				return (1);
+
+	}
+
+	else if (n < 0)
+
+	{
+
+			_putchar('-');
+
+				return (-1);
+
+	}
+
+	else
+
+	{
+
+			_putchar('0');
+
+				return (0);
+
+	}
+
+}
