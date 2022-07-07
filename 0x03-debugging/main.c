7a92929cfafd1412ce063234156912f5161d5cbb
Vera Aninakwah <vaykeys0112@gmail.com>
2022-07-07 14:48:09 -0700

    task0

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+void positive_or_negative(int i){
+	int n;
+
+srand(time(0));
+n = i
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+	printf("%d is positive\n", n);
+if (n == 0)
+	printf("%d is zero\n", n);
+if (n < 0)
+{
+	printf("%d is negative\n", n);
+}
+return (0);
+}
+
+/**
+ * main - debugging people
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return(0);
+}
