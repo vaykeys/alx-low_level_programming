d6fa6a92c99603b066e9ac2a8cf2aeffdf7308f1
Vera Aninakwah <vaykeys0112@gmail.com>
2022-07-07 23:59:26 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper -> this is a function to check uppercase alphas
+ * @c: an integer paramer
+ * Return: something but success
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
